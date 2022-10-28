0c93605762f325a5551d2d9451b6759ee4f54d02
sunny <sunnyroy8084401051@gmail.com>
2022-10-28 22:35:47 +0530

    Initial commit

---
+++ src/literals.java
@@ -0,0 +1,24 @@
+/*
+now we are going to start new series of java coding section , in this section we will see about literal.
+we will see the following literal in this block i.e = Integer literals, Floating-point literals, Boolean literals, character
+literal, String literals.
+now the question arise what do you mean by literals?
+ans: A constant value in java is created by using a literal representation of it. for example: 100 = integer ,
+98.6 = floating-pointer, x = character and etc....
+ */
+public class literals {
+    public static void main(String[] args) {
+
+        Integer a = 1000;
+        Float b = (float)0.1;
+        Boolean c = Boolean.TRUE;
+        Character d = 'C';
+        String e = "palluwa pgli";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+
+    }
+}
